8a630085ee8c68da120d08e0788715a155d3de4f
Vale <valerauko@gmail.com>
2026-05-15 12:25:44 +0900

    calico tries to connect on lo, exclude it

--- scripts/03-k0s.sh
+++ scripts/03-k0s.sh
@@ -5,3 +5,7 @@ K0S_VERSION="v1.35.3+k0s.0"
 
 curl -sSLf https://get.k0s.sh | K0S_VERSION="${K0S_VERSION}" sh -
 mkdir -p /etc/k0s
+
+cp ../config/k0s.yaml /etc/k0s/k0s.yaml
+
+k0s install controller -c /etc/k0s/k0s.yaml --enable-worker --no-taints --cri-socket remote:unix:///run/crio/crio.sock --force --start
